1a7359c85d5eb2ed657c7b4b32224276629faf41
John Cheng <github@johncheng.com>
2015-05-28 15:22:46 -0700

    testing what travis sez

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -2,31 +2,6 @@ require 'spec_helper'
 
 describe 'apache', :type => 'class' do
 
-  context "On a Debian OS with no package name specified" do
-    let :facts do
-      {
-        :osfamily => 'Debian'
-      }
-    end
-
-    it {
-      should contain_package('httpd').with( { 'name' => 'apache2' } )
-      should contain_service('httpd').with( { 'name' => 'apache2' } )
-    }
-  end
-
-  context "On a RedHat OS with no package name specified" do
-    let :facts do
-      {
-        :osfamily => 'RedHat'
-      }
-    end
-
-    it {
-      should contain_package('httpd').with( { 'name' => 'httpd' } )
-      should contain_service('httpd').with( { 'name' => 'httpd' } )
-    }
-  end
 
   context "On an unknown OS with no package name specified" do
     let :facts do
@@ -40,16 +15,5 @@ describe 'apache', :type => 'class' do
     }
   end
 
-  context "With a package name specified" do
-    let :params do
-      {
-        :package_name => 'apache'
-      }
-    end
 
-    it {
-      should contain_package('httpd').with( { 'name' => 'apache' } )
-      should contain_service('httpd').with( { 'name' => 'apache' } )
-    }
-  end
 end
